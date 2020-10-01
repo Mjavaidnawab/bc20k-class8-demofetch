5444d7027c3e9cb13ee4a0ef9a61698c86d1379e
Mjavaidnawab <57194178+Mjavaidnawab@users.noreply.github.com>
2020-10-01 14:58:14 +0400

    Initialize project using Create React App
    PIAIC Boot Camp Class 8 PIAIC : Get Data in JavaScript and React Fetch API

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React , { useEffect } from 'react';
+import { useState } from 'react';
+
+import './App.css';
+
+function App() {
+  let data = {title: "Waiting for Data"};
+  const [todo, setTodo] = useState({data});
+  const [isData, setData ] =useState(false);
+  const [isFetching, setFetching] =useState(false);
+
+  useEffect(  ()=>{
+async function fetchData(){
+  setFetching(true); //when it start the fetching data
+  const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+  console.log("The response is : ", response);
+
+  let data2 = await response.json();
+  setTodo(data2);
+  setFetching(false); //once it done the fetching data
+  //console.log("The data is : ", data);
+
+}
+fetchData();
+ 
+  
+},[isData]);if(isFetching){
+  return <div>Data Loading ....</div>
+}
+
+  return (
+    <div >
+     
+     <h1>PIAIC Boot Camp Class 8 PIAIC : Get Data in JavaScript and React Fetch API</h1>
+     <h2>Student : Muhammad Javaid Nawab </h2>
+  <span>Title: {todo.title}</span>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/PostList.js
@@ -0,0 +1,16 @@
+import React , { useEffect } from 'react';
+import { useState } from 'react';
+
+import './App.css';
+
+function PostList() {
+   return (
+    <div >
+        Post List
+      Assalam o Aleikum This is Fetch
+  <span>Title: {todo.title}</span>
+    </div>
+  );
+}
+
+export default PostList;
