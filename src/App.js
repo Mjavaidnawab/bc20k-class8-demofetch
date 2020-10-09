6955f13f1d643f6c7f5036c417156c24345b6ebf
Mjavaidnawab <57194178+Mjavaidnawab@users.noreply.github.com>
2020-10-09 12:48:12 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
 
   useEffect(  ()=>{
 async function fetchData(){
+  setData(true); // added after surge deploy error
   setFetching(true); //when it start the fetching data
   const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
   console.log("The response is : ", response);
